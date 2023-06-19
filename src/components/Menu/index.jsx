62e3ce53b0bfccfa8a24fe410814731aa262075c
Victor Mielke <vicemanu31@outlook.com>
2023-06-19 20:20:23 -0300

    menu mobile pronto

--- src/components/Menu/index.jsx
+++ src/components/Menu/index.jsx
@@ -6,37 +6,25 @@ import { useEffect, useState } from 'react'
 
 export default function Menu() {
 
-    const [larguraTela, setLarguraTela] = useState(window.innerWidth)
-
-    useEffect (()=> {
-        const handleResize = () => {
-            setLarguraTela(window.innerWidth)
-        }
-
-        window.addEventListener('resize', handleResize)
-
-    },[])
-
-
-    function gerarMenu() {
-        return(
-                <>
-                    <Logo/>
-                    <Pesquisa/>
-                    <nav>
-                        <Link to={'/'}>Home</Link>
-                        <Link to={'/artigos'}>Artigos</Link>
-                        {larguraTela}
-                    </nav>
-                </>
-            )
-
-    }
+    const [displayBlock, setDisplayBlock] = useState(false)
 
     return(
         <menu className="menu">
-         {larguraTela > 750 && gerarMenu}
-
+            <Logo/>
+            <Pesquisa/>
+            <nav>
+                <Link to={'/'}>Home</Link>
+                <Link to={'/artigos'}>Artigos</Link>
+            </nav>
+            <div className='menu--mobile'>
+                <button onClick={e => setDisplayBlock(!displayBlock)}><i className="bi bi-list"></i></button>
+                <nav className='menu_mobile--nav' style={{display: displayBlock ? "flex": "none" }}>
+                    <button onClick={e => setDisplayBlock(!displayBlock)}><i class="bi bi-x-circle"></i></button>
+                    <Logo/>
+                    <Link onClick={e => setDisplayBlock(!displayBlock)} to={'/'}>Home</Link>
+                    <Link onClick={e => setDisplayBlock(!displayBlock)} to={'/artigos'}>Artigos</Link>
+                </nav>
+            </div>
         </menu>
         // Fazer o nav do mobile
         
