6b7e157a4a5b6160e0cece9734c9409724875b28
Victor Mielke <vicemanu31@outlook.com>
2023-07-22 16:02:03 -0300

    ajeitando a estrutura dos dados

---
+++ src/redux/user/slice.jsx
@@ -0,0 +1,47 @@
+import { createSlice } from "@reduxjs/toolkit"
+
+const initialState = {
+    user: {
+        name: "",
+    }
+}
+
+export const userSlice = createSlice({
+    name: "user",
+    initialState,
+    reducers: {
+        createUser: (state, action) => {
+            
+            return {
+                ...state,
+                user: {
+                    name: action.payload.name
+                }
+            }
+        }
+    }
+})
+
+export const { createUser } = userSlice.actions;
+
+
+export default userSlice.reducer;
+
+
+/** 
+ * consumir dados
+ * const {} = useSelector(rootReducer => rootReducer.user)
+ * 
+ * 
+ */
+
+/**
+ * consumir ação
+ * 
+ * importa a ação
+ * 
+ * const dispatch = useDispatch();
+ * chama dispatch(ação({
+ * stato: mudança de status
+ * }))
+ */
