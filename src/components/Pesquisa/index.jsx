a16a0ba1aef234d1e4aa2fc7a1c038316e2e4983
Victor Mielke <vicemanu31@outlook.com>
2023-07-23 16:24:09 -0300

    ajeitando a barra de pesquisa

--- src/components/Pesquisa/index.jsx
+++ src/components/Pesquisa/index.jsx
@@ -9,7 +9,8 @@ export default function Pesquisa() {
     return(
         <form action={`/artigos/${dados === undefined ? "": dados}`}   className='form--pesquisa'>
             <input type="text" onChange={e => setDados(e.target.value)} />
-            <button type='submit'> <i className="bi bi-search"></i> </button>
+
+            <button type='submit'> <i className="bi bi-search"></i></button>
         </form>
     )
 
